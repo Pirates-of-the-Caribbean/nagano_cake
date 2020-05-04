4fd9125c37f142557f97db040b41e0acd1ba0d04
na-toyoshima <na.toyoshim@gmail.com>
2020-05-04 09:24:22 +0000

    [fix]db/schema.rb
    -mschemaファイルの修正。 not nullとdefault値追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_144325) do
+ActiveRecord::Schema.define(version: 2020_05_04_091026) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,21 +40,21 @@ ActiveRecord::Schema.define(version: 2020_04_30_144325) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "postcode"
-    t.string "address"
-    t.string "telephone_number"
-    t.boolean "validness"
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "postcode", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "telephone_number", default: "", null: false
+    t.boolean "validness", default: true, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "valid_flag"
+    t.boolean "valid_flag", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2020_04_30_144325) do
     t.integer "genre_id"
     t.integer "price"
     t.string "image_id"
-    t.integer "sale_status"
+    t.integer "sale_status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2020_04_30_144325) do
   create_table "order_items", force: :cascade do |t|
     t.integer "order_id"
     t.integer "item_id"
-    t.integer "production_status"
+    t.integer "production_status", default: 0
     t.integer "order_price"
     t.integer "quantity"
     t.datetime "created_at", null: false
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 2020_04_30_144325) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "status"
+    t.integer "status", default: 0
     t.integer "customer_id"
     t.string "postcode"
     t.string "address"
