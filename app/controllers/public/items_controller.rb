9f85957b79fc581c51d956cb4383225b5fade74c
tachikawatomoki <tttachikawatomoki@gmail.com>
2020-05-07 10:14:26 +0000

    item変更

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,22 @@
 class Public::ItemsController <  Public::Base
 	def index
-		@items = Item.all
 		@genres = Genre.all
+		if params[:genre_id]
+		@items=Item.where(genre_id: params[:genre_id])
+		else
+		@items = Item.all
+	end
 	end
 
 	def show
 		@genres = Genre.all
+		if params[:genre_id]
+		@items=Item.where(genre_id: params[:genre_id])
+		else
+		@items = Item.all
 		@cart_item = CartItem.new
 		@item=Item.find(params[:id])
+	end
 	end
 	
 end
