27a616e107c43be5c823621d9ca2d64fa61a0132
na-toyoshima <na.toyoshim@gmail.com>
2020-05-07 11:22:52 +0000

    [Add]view/admin/orders/index.html.erb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,23 @@
 class Order < ApplicationRecord
     has_many :order_items,dependent: :destroy
-    belongs_to :user
+    belongs_to :customer
+    enum status:{
+        入金待ち: 0,
+        入金確認: 1,
+        製作中: 2,
+        発送準備中: 3,
+        発送済み: 4
+    }
+    # def subtotal
+    #     order_item.order_price * order_item.quantity
+    # end
+    
+    # def total_quantity
+    #     total = 0
+    #     order_items.each do |order_item|
+    #         total += order_items.count
+    #     end
+    #     total
+    # end
+
 end
